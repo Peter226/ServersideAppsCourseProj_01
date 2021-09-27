68b6ccc2308695b5e910effdd3564a2b45a6da56
Hallgató (4-111-15) <4-111-15-hallgato@gamf.hu>
2021-09-27 14:52:42 +0200

    _

--- GAMF/GAMF/Models/Course.cs
+++ GAMF/GAMF/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace GAMF.Models
 {
     public class Course
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CourseId { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
